e1f17b54d4f602ab57298454ff8ff499eaedcaac
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2023-02-11 22:20:50 -0500

    feat: add perfil controller and update view

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/perfil', [App\Http\Controllers\PerfilController::class, 'edit'])->name('perfil');
+
+Route::post('/perfil', [App\Http\Controllers\PerfilController::class, 'update'])->name('perfil.update');
